22941629698402b06b08095a3594c53683838574
John Mathew D. Diño <dino_mathew@yahoo.com>
2021-10-14 02:59:39 +0800

    exploring asynchronous

---
+++ weather-app/app.js
@@ -0,0 +1,12 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log("2 second Timer")
+}, 2000)
+
+setTimeout(() =>{
+    console.log("0 Second Timer")
+},0)
+
+console.log('Stopping') 
+
