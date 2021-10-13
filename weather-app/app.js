256052eb41b2a86ce6875c0c851503972c7a3935
John Mathew D. Diño <dino_mathew@yahoo.com>
2021-10-14 03:00:36 +0800

    geocoding API using mapbox

--- weather-app/app.js
+++ weather-app/app.js
@@ -24,3 +24,11 @@ request({ url: url, json:true },(error, response) => {
     console.log(weather_descriptions+ ". It is currently "+ temperature + " degress out. It feels like "+feelslike+" degrees out.")
 
 })
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Bulan%20Sorsogon.json?access_token=pk.eyJ1IjoianVhbm1hdHRlbyIsImEiOiJja3VwdTJ3OHI0bzA4MndvOGtvZWhrY20wIn0.c2s4TtrNnvq0YfiXGoXp-g&limit=1'
+
+request({ url : geocodeURL, json:true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
+})
