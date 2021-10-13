029c1279619e0a25196b328b0e39c1ec2e82f9f8
John Mathew D. Diño <dino_mathew@yahoo.com>
2021-10-14 03:00:04 +0800

    weather-stack API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,26 @@
-console.log('Starting')
+// console.log('Starting')
 
-setTimeout(() => {
-    console.log("2 second Timer")
-}, 2000)
+// setTimeout(() => {
+//     console.log("2 second Timer")
+// }, 2000)
 
-setTimeout(() =>{
-    console.log("0 Second Timer")
-},0)
+// setTimeout(() =>{
+//     console.log("0 Second Timer")
+// },0)
 
-console.log('Stopping') 
+// console.log('Stopping') 
 
+// a3371e89db999a6f68a7a908b9195a56
+
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=a3371e89db999a6f68a7a908b9195a56&query=12.6732203,123.8749691'
+
+request({ url: url, json:true },(error, response) => {
+    // console.log(response.body.current.temperature)
+    const temperature = response.body.current.temperature
+    const feelslike = response.body.current.feelslike
+    const weather_descriptions = response.body.current.weather_descriptions[0]
+    console.log(weather_descriptions+ ". It is currently "+ temperature + " degress out. It feels like "+feelslike+" degrees out.")
+
+})
