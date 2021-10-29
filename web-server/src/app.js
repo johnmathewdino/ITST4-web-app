f87d68b6dc0373e74d65888b38b716aa871b2c8e
John Mathew D. Diño <dino_mathew@yahoo.com>
2021-10-30 01:11:11 +0800

    Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,43 @@
+const path = require('path')
 const express = require('express')
 
+
+console.log(__dirname)
+// D:\3rd Year\4th Year\Web\new\web-server\src
+
+// console.log(__filename)
+// D:\3rd Year\4th Year\Web\new\web-server\src\app.js
+
+console.log(path.join(__dirname, '../public'))
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
 
 // app.com
-app.get('', (req, res) =>{
-    res.send('<h1> Weather </h1>')
-})
+// app.get('', (req, res) =>{
+//     res.send('<h1> Weather </h1>')
+// })
 
 // app.com/help
-app.get('/help', (req, res) =>{
-    // res.send({
-    //     name: 'Mathew',
-    //     age: 21
-    // })
-    res.send([{
-        name: 'Mathew',
-    },{
-        name: 'Juan'
-    }])
-})
+// app.get('/help', (req, res) =>{
+//     // res.send({
+//     //     name: 'Mathew',
+//     //     age: 21
+//     // })
+//     res.send([{
+//         name: 'Mathew',
+//     },{
+//         name: 'Juan'
+//     }])
+// })
 
-// app.com/about
-app.get('/about', (req, res) =>{
-    res.send('<h1>About</h1>')
-})
+// // app.com/about
+// app.get('/about', (req, res) =>{
+//     res.send('<h1>About</h1>')
+// })
 
 // app.com/weather
 app.get('/weather', (req, res) =>{
