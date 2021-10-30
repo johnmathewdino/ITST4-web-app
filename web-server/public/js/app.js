30fb83243f6faae8406b96d666d22c833b291207
John Mathew D. Diño <dino_mathew@yahoo.com>
2021-10-31 01:45:15 +0800

    search form + design

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,39 @@
 console.log('JS Loaded')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) =>{
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response) =>{
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
-fetch('http://localhost:3000/weather?address=Bulan,Sorsogon').then((response) =>{
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+
+// message1.textContent= 'From JS'
+
+weatherForm.addEventListener('submit',(event) =>{
+    event.preventDefault()
+    const location = search.value
+
+    message1.textContent="Loading..."
+    message2.textContent=""
+
+
+    fetch('http://localhost:3000/weather?address='+ location).then((response) =>{
     response.json().then((data)=>{
         if (data.error){
             console.log(data.error)
+            message1.textContent=data.error
         }else{
-            console.log(data.location)
-            console.log(data.forecast)
+            message1.textContent=data.location
+            message2.textContent=data.forecast
+            // console.log(data.location)
+            // console.log(data.forecast)
         }
     })
+})
 })
